a01fb26cf429272be7765f5a93a25c97a42db1ba
Hono_coder <tkmc.prsector@outlook.jp>
2025-10-24 14:52:11 +0900

    14:52

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', () => {
-  const splash = document.getElementById('splash');
-  const main = document.getElementById('main');
+    const splash = document.getElementById('splash');
+    const main = document.getElementById('main');
 
   // 3秒後にスプラッシュをフェードアウト
-  setTimeout(() => {
-    splash.classList.add('fade-out');
     setTimeout(() => {
-      splash.style.display = 'none';
-      main.style.display = 'block';
-    }, 1000);
-  }, 3000);
+        splash.classList.add('fade-out');
+        setTimeout(() => {
+            splash.style.display = 'none';
+            main.style.display = 'block';
+        }, 1000);
+    }, 3000);
 });
